d5b6e5261c564db0ccf0b8f971f2fd8b181859b6
Hadrien Patte <hadrien.patte@datadoghq.com>
2025-01-10 15:26:16 +0100

    Return to last edit position when opening files

--- lvim.config.lua
+++ lvim.config.lua
@@ -3,6 +3,16 @@ vim.opt.shiftwidth = 4        -- Number of spaces per identation
 vim.opt.tabstop = 4           -- Number of visual spaces per TAB
 vim.opt.wrap = true           -- Wrap lines
 
+lvim.autocommands = {
+    { -- Return to last edit position when opening files
+        "BufReadPost",
+        {
+            pattern = { "*" },
+            command = [[if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]],
+        }
+    },
+}
+
 lvim.builtin.gitsigns.opts.current_line_blame = true
 lvim.builtin.gitsigns.opts.current_line_blame_formatter = "    <author>, <author_time:%Y-%m-%d> - <summary>"
 lvim.builtin.gitsigns.opts.current_line_blame_opts.delay = 100
